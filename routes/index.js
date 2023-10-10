d0e9b0ef58275e75a2bb8501e96e6f51e1d3c1af
aaahanamehrotra <e10991aaahana@dpsrkp.net>
2023-10-10 16:48:56 +0530

    fix ui

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,22 @@ const router = express.Router();
 
 const Product = require("../models/Product");
 
-router.get("/", (req, res) => res.render("welcome"));
+router.get("/", (req, res) => res.render("dashboard"));
 router.get("/about", (req, res) => res.render("about"));
+router.get("/checkout", ensureAuthenticated, (req, res) =>
+  res.render("checkout")
+);
 router.get("/book", ensureAuthenticated, (req, res) => {
   const data = req.user;
   console.log(data);
   res.render("book", { user: req.user });
 });
 
-router.get("/dashboard", ensureAuthenticated, (req, res) =>
-  res.render("dashboard", {
-    name: req.user.name,
-  })
-);
+// router.get("/dashboard", ensureAuthenticated, (req, res) =>
+//   res.render("dashboard", {
+//     name: req.user.name,
+//   })
+// );
 
 router.get("/profile", ensureAuthenticated, (req, res) =>
   res.render("profile", {
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
+const User = require("../models/User");
 
 router.get("/", async (req, res) => {
   try {
     let products = await Product.find();
     // console.log(products);
     res.render("products", { data: products });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+const buying = async (product_id) => {
+  const product = await Product.findById(product_id);
+  const stock = product.stock;
+  console.log("dfghjkl");
+  if (stock === 0) {
+    return 0;
+  }
+  const updated = await Product.updateOne(
+    { _id: product_id },
+    { $set: { stock: stock - 1 } }
+  );
+};
+
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body.id);
+    const out_of_stock = await buying(req.body.id);
+    if (out_of_stock === 0) {
+      res.redirect("/outofstock");
+      return;
+    }
+    console.log("hi");
+    res.redirect("/checkout");
   } catch (err) {
     res.status(500).json(err);
   }
